04396ce0b47b2ce797d984f33e9f36c00c493b77
joesmitty2285 <joesmitty@me.com>
2024-06-14 08:47:14 -0500

    Initialize

---
+++ Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Joseph Smith on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
